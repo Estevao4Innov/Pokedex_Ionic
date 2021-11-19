b3b3856540f5852afa1812a2ce6edf2ad4a52cfd
kaynnin <estevaosoares147@gmail.com>
2021-11-19 11:42:08 -0300

    pokedex finished

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll} from '@ionic/angular';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  offset= 0;
+  pokemons = [];
 
-  constructor() {}
+  @ViewChild(IonInfiniteScroll) infinite: IonInfiniteScroll;
 
+  constructor(private pokemonService: PokemonService) {}
+
+
+  ngOnInit() {
+    this.loadPokemon();
+  }
+
+  loadPokemon(loadMore = false, event?){
+    if (loadMore){
+      this.offset += 25;
+    }
+
+    this.pokemonService.getPokemon(this.offset).subscribe ( res => {
+      this.pokemons = [...this.pokemons, ...res];
+
+      if(event){
+        event.target.complete();
+      }
+
+      if(this.offset === 1000){
+        this.infinite.disabled = true;
+      }
+    })
+  }
+
+  onSearchChange(e){
+    let value = e.detail.value
+
+    if(value == ''){
+      this.offset = 0;
+      this.loadPokemon();
+      return
+    }
+
+    this.pokemonService.findPokemon(value).subscribe(res =>{
+      this.pokemons = [res];
+    }, err => {
+      this.pokemons = []; 
+    })
+  }
 }
